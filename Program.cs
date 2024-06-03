5316f04842715eef2212983b7c9ba9f60f9d4f7b
GUSTAVO A S VALLE VALLE <gustavo.vallechina@gmail.com>
2024-06-03 19:36:29 -0300

    palindromo em c#

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Palíndromo_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("Digite:");
+                string palavra = Console.ReadLine();
+                string result = string.Empty;
+                int tamanhoPalavra = palavra.Length - 1;
+
+                for (int i = 0; i < palavra.Length; i++)
+                {
+                    result += palavra[tamanhoPalavra - i].ToString();
+                }
+
+                if (palavra == result)
+                    Console.WriteLine("É palindromo");
+                else
+                    Console.WriteLine("Não é palindromo");
+           
+        }
+      
+       
+    }
+}
